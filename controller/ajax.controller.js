4f6cf472abe983152f116ecbfa79fb945de6d211
I4104 <tuicontho@gmail.com>
2023-04-09 14:41:02 +0700

    Update ajax.controller.js

--- controller/ajax.controller.js
+++ controller/ajax.controller.js
@@ -3,7 +3,6 @@ const filmModel = require("../models/films.model");
 const axios = require('axios');
 const moment = require('moment');
 const { Op } = require('../config');
-const sharp = require('sharp');
 const fs = require('fs');
 
 
@@ -62,29 +61,55 @@ class AjaxController {
 
             await Promise.all(results.map(async function(item) {
 
-                const response_thumb = await axios.get("https://img.ophim1.com/uploads/movies/"+ item.thumb_url.split("/").pop(), {
-                    responseType: 'arraybuffer'
+                // const response_thumb = await axios.get("https://img.ophim1.com/uploads/movies/"+ item.thumb_url.split("/").pop(), {
+                //     responseType: 'arraybuffer'
+                // });
+                // const response_poster = await axios.get("https://img.ophim1.com/uploads/movies/"+ item.poster_url.split("/").pop(), {
+                //     responseType: 'arraybuffer'
+                // });
+
+                // const res_thumb = await sharp(response_thumb.data)
+                //     .jpeg({ quality: 80 })
+                //     .toBuffer();
+
+                // const res_poster = await sharp(response_poster.data)
+                //     .jpeg({ quality: 80 })
+                //     .toBuffer();
+
+                axios({
+                    url: "https://img.ophim1.com/uploads/movies/"+ item.thumb_url.split("/").pop(),
+                    responseType: 'stream',
+                }).then(response => {
+                    response.data.pipe(fs.createWriteStream('../public/uploads/' + item.thumb_url.split("/").pop()));
+                    response.data.on('end', () => {
+                        console.log('Image downloaded successfully');
+                    });
+                    
+                    await filmModel.update({
+                        thumb_url: '/uploads/' + item.thumb_url.split("/").pop(),
+                    }, {
+                        where: { id: item.id }
+                    });
+                }).catch(error => {
+                    console.error(error);
                 });
-                const response_poster = await axios.get("https://img.ophim1.com/uploads/movies/"+ item.poster_url.split("/").pop(), {
-                    responseType: 'arraybuffer'
-                });
+                
+                axios({
+                    url: "https://img.ophim1.com/uploads/movies/"+ item.poster_url.split("/").pop(),
+                    responseType: 'stream',
+                }).then(response => {
+                    response.data.pipe(fs.createWriteStream('../public/uploads/' + item.poster_url.split("/").pop()));
+                    response.data.on('end', () => {
+                        console.log('Image downloaded successfully');
+                    });
 
-                const res_thumb = await sharp(response_thumb.data)
-                    .jpeg({ quality: 80 })
-                    .toBuffer();
-
-                const res_poster = await sharp(response_poster.data)
-                    .jpeg({ quality: 80 })
-                    .toBuffer();
-
-                fs.writeFileSync('../uploads/' + item.thumb_url.split("/").pop(), res_thumb);
-                fs.writeFileSync('../uploads/' + item.poster_url.split("/").pop(), res_poster);
-
-                await filmModel.update({
-                    thumb_url: '/uploads/' + item.thumb_url.split("/").pop(),
-                    poster_url: '/uploads/' + item.poster_url.split("/").pop(),
-                }, {
-                    where: { id: item.id }
+                    await filmModel.update({
+                        poster_url: '/uploads/' + item.poster_url.split("/").pop(),
+                    }, {
+                        where: { id: item.id }
+                    });
+                }).catch(error => {
+                    console.error(error);
                 });
             }));
 
