064064fb40e082e8dbdb63bc8efdb1fb18fe6030
I 4 1 0 4 <58819071+i4104x@users.noreply.github.com>
2023-04-11 00:50:44 +0700

    Update ajax.controller.js

--- controller/ajax.controller.js
+++ controller/ajax.controller.js
@@ -418,19 +418,20 @@ class AjaxController {
                     var episode_total = (data.movie.episode_total != null) ? data.movie.episode_total : 0;
                     var showtimes = (data.movie.showtimes != null) ? data.movie.showtimes : "";
                     let modified = moment(data.movie.modified.time).format('Y-MM-DD H:mm:ss');
+                    if (item.episode_current != episode_current) {
+                        await filmModel.update({
+                            status: data.movie.status,
+                            episode_current: episode_current,
+                            episode_total: episode_total,
+                            showtimes: showtimes,
+                            modified: modified,
+                            m3u8: JSON.stringify(data.episodes),
+                        }, {
+                            where: { id: item.id }
+                        });
 
-                    await filmModel.update({
-                        status: data.movie.status,
-                        episode_current: episode_current,
-                        episode_total: episode_total,
-                        showtimes: showtimes,
-                        modified: modified,
-                        m3u8: JSON.stringify(data.episodes),
-                    }, {
-                        where: { id: item.id }
-                    });
-
-                    await logs.create({ content: "Update Series: " + item.title + " - Current episode: " + episode_current });
+                        await logs.create({ content: "Update Series: " + item.title + " - Current episode: " + episode_current });
+                    }
                 }));
 
             }
