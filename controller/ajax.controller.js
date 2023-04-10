7070356e0ed69cd9a3210bdbac2324450f90a6d2
I 4 1 0 4 <58819071+i4104x@users.noreply.github.com>
2023-04-10 20:16:44 +0700

    Update ajax.controller.js

--- controller/ajax.controller.js
+++ controller/ajax.controller.js
@@ -14,7 +14,7 @@ class AjaxController {
         if (page < 1) {
             page = 1
         } 
-        const perPage = 41; 
+        const perPage = 45; 
         const offset = (page - 1) * perPage; 
 
         try {
