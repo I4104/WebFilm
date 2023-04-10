429f427c21ee266642568a4fb02c5695662fbe95
I 4 1 0 4 <58819071+i4104x@users.noreply.github.com>
2023-04-11 01:11:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,10 @@ const { Sequelize, DataTypes, Op } = require('sequelize');
 const sequelize = new Sequelize('admin', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
-    logging: false
+    logging: false,
+    dialectOptions: {
+        timezone: '+07:00', // Thay đổi thành giờ địa phương của bạn
+    },
 });
 
 const keySecret = "I4104@KnSea";
