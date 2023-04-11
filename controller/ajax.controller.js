1ed0a51a41f548da55328a8b8b5deeb7cc031e3d
I 4 1 0 4 <58819071+i4104x@users.noreply.github.com>
2023-04-11 14:06:15 +0700

    Update Search

--- controller/ajax.controller.js
+++ controller/ajax.controller.js
@@ -20,17 +20,22 @@ class AjaxController {
 
         try {
             var html = "";
+            var slug = req.params.search.normalize('NFD')
+                    .replace(/[\u0300-\u036f]/g, '')
+                    .replace(/đ/g, 'd').replace(/Đ/g, 'D').replace(/\s+/g, '-')
+                    .replace(/-+/g, '-');
+
             const results = await filmModel.findAndCountAll({ 
                 where: {
                     [Op.or]: {
                         title: {
                             [Op.like]: '%'+ req.params.search +'%'
                         },
-                        tag: {
+                        tags: {
                             [Op.like]: '%'+ req.params.search +'%'
                         },
                         slug: {
-                            [Op.like]: '%'+ req.params.search +'%'
+                            [Op.like]: '%'+ slug +'%'
                         },
                     }
                 },
